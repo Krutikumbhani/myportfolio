0650d7e94c20aadca2be89e7fba1044e5b8587c9
Kruti kumbhani <krutikumbhani396@gmail.com>
2025-03-24 13:43:50 +0530

    added scroll page navigation

---
+++ component/Scrollpage.jsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+const pages = ['/aboutpage', '/mywork', '/contactpage'];
+
+export default function ScrollNavigator({ children }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleScroll = (event) => {
+      const delta = event.deltaY;
+      const currentPath = window.location.pathname;
+      const currentIndex = pages.indexOf(currentPath);
+      
+      if (delta > 0 && currentIndex < pages.length - 1) {
+        setTimeout(() => router.push(pages[currentIndex + 1]), 2000);
+      } else if (delta < 0 && currentIndex > 0) {
+        setTimeout(() => router.push(pages[currentIndex - 1]), 2000);
+      }
+    };
+
+    window.addEventListener('wheel', handleScroll);
+    return () => window.removeEventListener('wheel', handleScroll);
+  }, [router]);
+
+  return <div>{children}</div>;
+}
